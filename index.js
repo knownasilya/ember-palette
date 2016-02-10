a8e84cb47f4aea1aacc6231ed791770e6dc07bfb
Ilya Radchenko <ilya@burstcreations.com>
2016-02-10 13:58:44 -0500

    Actually fix nested usage
    Also add a tip for consumers

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ module.exports = {
   included: function(app, parentAddon) {
     var target = (parentAddon || app);
 
+    // necessary for nested usage
+    // parent addon should call `this._super.included.apply(this, arguments);`
+    if (target.app) {
+      target = target.app;
+    }
+
+    this.app = target;
     this._super.included.apply(this, arguments);
 
     target.import('vendor/ember-palette.css');
